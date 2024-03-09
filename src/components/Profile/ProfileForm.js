7cf2647a70999f3dc192fec34c270b741fbe361e
bhanu567 <bhanupjha5465@gmail.com>
2024-03-09 12:22:50 +0530

    Added the functionality of change password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,51 @@
-import classes from './ProfileForm.module.css';
+import { useRef } from "react";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const myKey = "";
+  const changePasswordRef = useRef();
+  const changePasswordHandler = async (e) => {
+    e.preventDefault();
+    const newPassword = changePasswordRef.current.value;
+    if (newPassword.length > 7) {
+      try {
+        const myTokenId = localStorage.getItem("token");
+        const response = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:update?key=" +
+            myKey,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              idToken: myTokenId,
+              password: newPassword,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        if (!data.error) alert("Password Updated Successfully");
+        else throw data.error;
+      } catch (error) {
+        alert(error.message);
+      }
+    } else {
+      alert("Enter Password Should have more than 7 characters");
+    }
+  };
   return (
     <form className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={changePasswordRef} />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button onClick={changePasswordHandler}>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
